98ed41cdf0762e02b7952870209de31a2a847284
EvanSimpson <esimpson.rescue@gmail.com>
2013-08-11 04:16:15 -0400

    configuring app

--- app.js
+++ app.js
@@ -4,32 +4,41 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
+var mongodb = require("mongodb"),
+	mongoserver = new mongodb.Server('localhost', mongodb.Connection.DEFAULT_PORT, {auto_reconnect:true}),
+	db_connector = new mongodb.Db('BrowsingData', mongoserver, {safe:true});
+
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+db_connector.open(function(err, db){
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+	// all environments
+	app.set('port', process.env.PORT || 4000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	// development only
+	if ('development' == app.get('env')) {
+	  app.use(express.errorHandler());
+	}
+
+	app.post('/save', function(req, resp){
+		console.log(req.body);
+	});
+
+	http.createServer(app).listen(app.get('port'), function(){
+	  console.log('Express server listening on port ' + app.get('port'));
+	});
+
+
 });
